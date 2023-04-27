00c8a1671b5d12e31bd51921bb1a491f45874657
Marco Jurupe <marco.jurupe@gmail.com>
2023-04-26 22:46:44 -0500

    index.js modified successfully

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+  let findBroncoWin = record.find(({ result }) => record.result === "W");
+  if (findBroncoWin) {
+    return findBroncoWin["year"];
+  }
+}
